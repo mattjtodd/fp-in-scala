b146d2c7c7431a6838fcbcfefecca78ac8e10dbd
mattjtodd <matt.j.todd@gmail.com>
2015-04-17 23:08:14 +0100

    Added answers to some more questions

--- src/Chapter3.scala
+++ src/Chapter3.scala
@@ -60,15 +60,11 @@ object Chapter3 {
 
     // question 10 implement fold left to be tail recursive
     @tailrec
-    def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = l match {
+    def foldLeft[A, B](l: List[A], z: B)(f: (B, A) => B): B = l match {
       case Nil => z
       case Cons(h, t) => foldLeft(t, f(z, h))(f)
     }
-    println(foldLeft(List(1, 2, 3, 4), 0)(
-      (x, xs) => {
-        println("dd34d3" + x + " " + xs)
-        x * xs
-      }))
+    println(foldLeft(List(1, 2, 3, 4), 0)(_ * _))
 
     // question 11 implement sum, product and count using foldLeft
     def sumFl(l: List[Int]) =
@@ -87,12 +83,12 @@ object Chapter3 {
     // question 12 - Write a function that returns the reverse of a list (so given List(1,2,3) it returns List(3,2,1)).
     // See if you can write it using a fold.
     def reverse[A](l: List[A]): List[A] = {
-      foldLeft(l, Nil:List[A])((x, xs) => Cons(xs, x))
+      foldLeft(l, Nil: List[A])((x, xs) => Cons(xs, x))
     }
     println(reverse(List(1, 2, 3)))
 
     // question 13 - Can you write foldLeft in terms of foldRight? How about the other way around?
-    def foldLeftFr[A,B](l: List[A], z: B)(f: (B, A) => B): B = {
+    def foldLeftFr[A, B](l: List[A], z: B)(f: (B, A) => B): B = {
       foldRight(l, z)((x, xs) => f(xs, x))
     }
 
@@ -105,7 +101,7 @@ object Chapter3 {
     println(foldRightFl(List(1, 2, 3), 0)(_ + _))
 
     // question 14 - Implement append in terms of either foldLeft or foldRight.
-    def append[A](l: List[A], app:List[A]) =
+    def append[A](l: List[A], app: List[A]) =
       foldRight(l, app)(Cons(_, _))
 
     println(append(List(1, 2, 3), List(4)))
@@ -114,49 +110,111 @@ object Chapter3 {
     // linear in the total length of all lists. Try to use functions we have already defined.
 
     def concat[A](l: List[List[A]]): List[A] = {
-      foldLeft(l, Nil:List[A])(append)
+      foldLeft(l, Nil: List[A])(append)
     }
 
     println(concat(List(List(1, 2), List(3, 4), List(5, 6), List(7), List())));
 
     // question 16 - Write a function that transforms a list of integers by adding 1 to each element.
     // (pure function only!!!)
-    def addOne(l:List[Int]) =
-      foldRight(l, Nil:List[Int])((h, t) => Cons(h + 1, t))
+    def addOne(l: List[Int]) =
+      foldRight(l, Nil: List[Int])((h, t) => Cons(h + 1, t))
 
     println(addOne(List(1, 2, 3, 4)))
 
     // question 17 - Write a function that turns each value in a List[Double] into a String.
-    def asString(l:List[Double]): List[String] =
-      foldRight(l, Nil:List[String])((h, t) => Cons(h.toString, t))
+    def asString(l: List[Double]): List[String] =
+      foldRight(l, Nil: List[String])((h, t) => Cons(h.toString, t))
 
     println(asString(List(1, 2, 3, 4, 5)))
 
     // question 18 - Write a function map, that generalizes modifying each element in a list while maintaining the
     // structure of the list
-    def map[A, B](l:List[A], f:A => B): List[B] =
-      foldRight(l, Nil:List[B])((h, t) => Cons(f(h), t))
+    def map[A, B](l: List[A], f: A => B): List[B] =
+      foldRight(l, Nil: List[B])((h, t) => Cons(f(h), t))
 
     println(map[String, Int](List("a", "aa", "aaa", "aaaa"), x => x.length))
 
     // question 19 - Write a function filter that removes elements from a list unless they satisfy a given predicate.
     // Use it to remote all odd numbers from a List[Int].
-    def filter[A](l:List[A], f:A => Boolean): List[A] = l match {
+    def filter[A](l: List[A], f: A => Boolean): List[A] = l match {
       case Cons(x, xs) if f(x) => Cons(x, filter(xs, f))
       case Cons(_, xs) => filter(xs, f)
       case Nil => l
     }
 
-    def filterV2[A](l:List[A], f:A => Boolean): List[A] =
-      foldRight(l, Nil:List[A])((h, t) => if (f(h)) Cons(h, t) else t)
+    def filterV2[A](l: List[A], f: A => Boolean): List[A] =
+      foldRight(l, Nil: List[A])((h, t) => if (f(h)) Cons(h, t) else t)
 
     println(filter[Int](List(1, 2, 3, 4), x => x == 3))
     println(filterV2[Int](List(1, 2, 3, 4), x => x == 3))
 
     // question 20 - Write a function flatMap, that works like map except that the function given will return a list
-    // instead of a single result, and that list should be inserted into the final resulting list.
-//    def flatMap[A,B](l: List[A])(f: A => List[B]): List[B] {
-//
-//    }
+    // instead of a single result, and that list should be inserted into the final resulting list. e.g.
+    // flatMap(List(1,2,3))(i => List(i,i)) should result in List(1,1,2,2,3,3).
+    def flatMap[A, B](l: List[A])(f: A => List[B]): List[B] =
+      foldRight(l, Nil: List[B])((h, t) => append(f(h), t))
+
+    println(flatMap(List(1, 2, 3, 4))(i => List(i, i)))
+
+    // question 21 - Can you use flatMap to implement filter?
+    def filterFM[A](l: List[A])(f: A => Boolean): List[A] =
+      flatMap(l)(i => if (f(i)) List(i) else List())
+
+    println(filterFM(List(1, 2, 3, 4, 5))(x => x == 4))
+
+    // question22 - : Write a function that accepts two lists and constructs a new list by adding corresponding elements.
+    // For example, List(1,2,3) and List(4,5,6) becomes List(5,7,9).
+    def addCoresp(l: List[Int], k: List[Int]): List[Int] = (l, k) match {
+      case (_, Nil) => Nil
+      case (Nil, _) => Nil
+      case (Cons(h1, t1), Cons(h2, t2)) => Cons(h1 + h2, addCoresp(t1, t2))
+    }
+
+    println(addCoresp(List(1, 2, 3), List(1, 2, 3)))
+
+    // question23 - Generalize the function you just wrote so that it's not specific to integers or addition.
+    def addCorespGen[A, B](l: List[A], k: List[A])(f: (A, A) => B): List[B] = (l, k) match {
+      case (_, Nil) => Nil
+      case (Nil, _) => Nil
+      case (Cons(h1, t1), Cons(h2, t2)) => Cons(f(h1, h2), addCorespGen(t1, t2)(f))
+    }
+
+    println(addCorespGen(List(1, 2, 3), List(1, 2, 3))(_ - _))
+
+    // question 24 -
+    def hasSubsequence[A](l: List[A], sub: List[A]): Boolean = l match {
+      case Nil => false
+      case Cons(_, t) => (checkSub(l, sub) || hasSubsequence(t, sub))
+    }
+
+    def checkSub[A](l: List[A], sub: List[A]): Boolean = (l, sub) match {
+      case (Cons(h1, _), Cons(h2, Nil)) => h1 == h2
+      case (Cons(h1, t1), Cons(h2, t2)) => if (h1 == h2) checkSub(t1, t2) else false
+    }
+
+    println(checkSub(List(1, 2, 3, 4, 5), List(4, 5)))
+
+
+    sealed trait Tree[+A]
+    case class Leaf[A](value: A) extends Tree[A]
+    case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
+
+    // question 25 - Write a function size that counts the number of nodes in a tree.
+    def treeSize[A](tree: Tree[A]): Int = tree match {
+      case Leaf(_) => 1
+      case Branch(x, y) => treeSize(x) + 1 + treeSize(y)
+    }
+
+    println(treeSize(Branch(Leaf(3), Leaf(3))))
+
+    // question 26 - Write a function maximum that returns the maximum element in a Tree[Int]
+    def treeMaxLeaf(tree: Tree[Int]): Int = tree match {
+        case Leaf(x) => x
+        case Branch(x, y) => treeMaxLeaf(x) max treeMaxLeaf(y)
+    }
+
+    println(treeMaxLeaf(Leaf(10)))
+    println(treeMaxLeaf(Branch(Leaf(10), Leaf(20))))
   }
 }
