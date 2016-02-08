d7ab1f3bce58aae21e38ef712bee5b7acaaac31d
mattjtodd <matt.j.todd@gmail.com>
2016-02-08 21:38:48 +0000

    Compliles with sbt

--- build.sbt
+++ build.sbt
@@ -1,9 +1,11 @@
 lazy val commonSettings = Seq(
   organization := "com.mattjtodd",
   version := "0.1.0",
-  scalaVersion := "2.11.6"
+  scalaVersion := "2.11.7"
 )
 
 lazy val root = (project in file("."))
   .settings(commonSettings: _*)
   .settings(name := "fp-in-scala")
+
+libraryDependencies += "org.scalacheck" %% "scalacheck" % "1.12.5" % "test"
--- src/main/scala/chapter3/Chapter3.scala
+++ src/main/scala/chapter3/Chapter3.scala
@@ -1,9 +1,40 @@
 package chapter3
 
-/**
- * Created by utuba on 05/04/15.
- */
+import scala.annotation.tailrec
+
 object Chapter3 {
+
+  // This list trait
+  sealed trait List[+A]
+
+  case object Nil extends List[Nothing]
+
+  case class Cons[+A](head: A, tail: List[A]) extends List[A]
+
+  object List {
+    def sum(ints: List[Int]): Int = ints match {
+      case Nil => 0
+      case Cons(x, xs) => x + sum(xs)
+    }
+
+    def product(ds: List[Double]): Double = ds match {
+      case Nil => 1.0
+      case Cons(0.0, _) => 0.0
+      case Cons(x, xs) => x * product(xs)
+    }
+
+    def apply[A](as: A*): List[A] =
+      if (as.isEmpty) Nil
+      else Cons(as.head, apply(as.tail: _*))
+  }
+
+  // The tree trait
+  sealed trait Tree[+A]
+  case class Leaf[A](value: A) extends Tree[A]
+  case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
+
+  //
+
   val question1 = List(1, 2, 3, 4, 5) match {
     case Cons(x, Cons(2, Cons(4, _))) => x
     case Nil => 42
@@ -22,6 +53,35 @@ object Chapter3 {
 
   def product2(l: List[Double]) =
     foldRight(l, 1.0)(_ * _)
+
+  def tail[A](list: List[A]): List[A] = list match {
+    case Cons(_, xs) => xs
+    case _ => ???
+  }
+
+  @tailrec
+  def drop[A](list: List[A], number: Int): List[A] = list match {
+    case Cons(_, xs) if number >= 1 =>
+      drop(xs, number - 1)
+    case _ => list
+  }
+
+  @tailrec
+  def dropWhile[A](list: List[A], func: A => Boolean): List[A] = list match {
+    case Cons(x, xs) if func(x) =>
+      dropWhile(xs, func)
+    case _ => list
+
+  }
+
+  def setHead[A](list: List[A], head: A): List[A] = {
+    Cons(head, list)
+  }
+
+  def init[A](list: List[A]): List[A] = list match {
+    case Cons(x, Nil) => Nil
+    case Cons(x, xs) => Cons(x, init(xs))
+  }
 
   def main(args: Array[String]) {
     // question 1 - the value is three because the third case is the first match
@@ -191,10 +251,6 @@ object Chapter3 {
 
     println(checkSub(List(1, 2, 3, 4, 5), List(4, 5)))
 
-
-    sealed trait Tree[+A]
-    case class Leaf[A](value: A) extends Tree[A]
-    case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
     // question 25 - Write a function size that counts the number of nodes in a tree.
     def treeSize[A](tree: Tree[A]): Int = tree match {
