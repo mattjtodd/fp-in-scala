85af85166eafc0d2076767c31f83bb50ee1d5bc7
mattjtodd <matt.j.todd@gmail.com>
2016-02-09 22:31:36 +0000

    Added 2.3 answer and test

--- src/main/scala/chapter2/Chapter2.scala
+++ src/main/scala/chapter2/Chapter2.scala
@@ -16,7 +16,7 @@ object Chapter2 {
   }
 
   /**
-    * Exeecise 2.2
+    * Exercise 2.2
     */
   def isSorted[A] (as: Array[A], orderd: (A, A) => Boolean): Boolean = {
     @tailrec
@@ -24,5 +24,12 @@ object Chapter2 {
       if (!result || n >= as.length) result else loop(n + 1, orderd.apply(as(n-1), as(n)))
     }
     loop(1, true)
+  }
+
+  /**
+    * Exercise 2.3
+    */
+  def curry[A, B, C] (f: (A, B) => C): A => (B => C) = {
+    a => b => f(a, b)
   }
 }
--- src/test/scala/chapter2/Chapter2$Test.scala
+++ src/test/scala/chapter2/Chapter2$Test.scala
@@ -23,4 +23,11 @@ class Chapter2$Test extends UnitSpec {
   "Is sorted" should "be true" in {
     assert(isSorted(Array(10, 50, 300), (one: Int, two: Int) => one < two))
   }
+
+  "When Integer Add Function Curried apply" should "be ten" in {
+    val addCurry = curry((a: Int, b: Int) => a + b)
+    val addFive = addCurry(5)
+
+    assert(addFive(5) == 10)
+  }
 }
