ddafa9abb61945fce26db680a2a835770bbe0912
Matt Todd <matt.todd@blackcatsolutions.co.uk>
2017-01-19 08:20:21 +0000

    Added Option and lift QAs

--- src/main/scala/chapter4/Chapter4.scala
+++ src/main/scala/chapter4/Chapter4.scala
@@ -30,13 +30,35 @@ object Chapter4 {
 
   case object None extends Option[Nothing]
 
-  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = ???
-//    a.flatMap(_ => b).flatMap(_ => Some(f(a, b)))
-//  }
+  /**
+    * 4.3 Write a generic function map2 that combines two Option values using a binary function.
+    * If either Option value is None, then the return value is too. Here is its signature:
+    */
+  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = {
+    a.flatMap(_a => b.flatMap(_b => Some(f(_a, _b))))
+  }
 
+  /**
+    * 4.4 Write a function sequence that combines a list of Options into one Option containing a list of all the
+    * Some values in the original list. If the original list contains None even once, the result of the function
+    * should be None; otherwise the result should be Some with a list of all the values.
+    */
   def sequence[A](a: List[Option[A]]): Option[List[A]] = {
     a.foldRight[Option[List[A]]](Some(Nil))((next, prev) => map2(next, prev)(_::_))
   }
 
-  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = ???
+  /**
+    * 4.5 Implement this function. It’s straightforward to do using map and sequence, but try for a more efficient
+    * implementation that only looks at the list once.
+    */
+  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = {
+    a.foldRight[Option[List[B]]](Some(Nil))((next, prev) => map2(f(next), prev)(_::_))
+  }
+
+  /**
+    * 4.5b In fact, implement sequence in terms of traverse.
+    */
+  def sequenceTraverse[A](a: List[Option[A]]): Option[List[A]] = {
+    traverse(a)(value => value)
+  }
 }
--- src/test/scala/chapter4/OptionTest.scala
+++ src/test/scala/chapter4/OptionTest.scala
@@ -1,16 +1,69 @@
 package chapter4
 
-import chapter4.Chapter4.{Option, Some}
+import chapter4.Chapter4.{None, Some, map2, sequence, traverse, sequenceTraverse}
 
 import org.scalatest._
 
-/**
-  * Created by utuba on 25/12/2016.
-  */
 class OptionTest  extends FlatSpec with Matchers with
   OptionValues with Inside with Inspectors {
 
-  "map with Some(4) add 4 " should "Some(8)" in  {
+  "4.0 map with Some(4) add 4 " should "Some(8)" in  {
     assert(Some(4).map(_ + 4) == Some(8))
+  }
+
+  "4.3 map2 None, None " should "be None" in  {
+    assert(map2(None, None)((a, b) => a) == None)
+  }
+
+  "4.3 map2 Some('Test'), None " should "be None" in  {
+    assert(map2(Some("Test"), None)((a, b) => a) == None)
+  }
+
+  "4.3 map2 Some('Test1'), Some('Test2') " should "be Some('Test1')" in  {
+    assert(map2(Some("Test1"), Some("Test2"))((a, _) => a) == Some("Test1"))
+  }
+
+  "4.4 sequence List(None) " should "be None" in  {
+    assert(sequence(List(None)) == None)
+  }
+
+  "4.4 sequence List(Some('Test')) " should "be Some(List('Test'))" in  {
+    assert(sequence(List(Some("Test"))) == Some(List("Test")))
+  }
+
+  "4.4 sequence List(Some('Test'), None) " should "be None" in  {
+    assert(sequence(List(Some("Test"), None)) == None)
+  }
+
+  "4.4 sequence List(Some('Test'), Some('Test')) " should "be Some(List('Test', 'Test'))" in  {
+    assert(sequence(List(Some("Test"), Some("Test"))) == Some(List("Test", "Test")))
+  }
+
+  "4.5 traverse List(1, 2, 3) with always Some " should "be Some(List(1, 2, 3))" in  {
+    assert(traverse(List(1, 2, 3))(Some(_)) == Some(List(1, 2, 3)))
+  }
+
+  "4.5 traverse List(1, 2, 3) with always None " should "be None" in  {
+    assert(traverse(List(1, 2, 3))(_ => None) == None)
+  }
+
+  "4.5 traverse List(1, 2, 3) when >= 2 None " should "be None" in  {
+    assert(traverse(List(1, 2, 3))(value => if (value >= 2) None else Some(value)) == None)
+  }
+
+  "4.5b sequenceTraverse List(None) " should "be None" in  {
+    assert(sequenceTraverse(List(None)) == None)
+  }
+
+  "4.5b sequenceTraverse List(Some('Test')) " should "be Some(List('Test'))" in  {
+    assert(sequenceTraverse(List(Some("Test"))) == Some(List("Test")))
+  }
+
+  "4.5b sequenceTraverse List(Some('Test'), None) " should "be None" in  {
+    assert(sequenceTraverse(List(Some("Test"), None)) == None)
+  }
+
+  "4.5b sequenceTraverse List(Some('Test'), Some('Test')) " should "be Some(List('Test', 'Test'))" in  {
+    assert(sequenceTraverse(List(Some("Test"), Some("Test"))) == Some(List("Test", "Test")))
   }
 }
